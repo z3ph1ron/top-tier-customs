af576dfe09ffc7cc8063c7b3c5e053575d0196ea
Sami Munir <sami.zephiron@gmail.com>
2025-11-11 13:48:37 -0500

    implementing Product model schema

---
+++ services/catalog/src/models/Product.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const variantSchema = new mongoose.Schema({
+  sku: {
+    type: String,
+  },
+  attrs: {
+    type: Object,
+    default: {},
+  },
+  priceDelta: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const productSchema = new mongoose.Schema(
+  {
+    slug: {
+      type: String,
+      unique: true,
+      index: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      index: "text",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    media: [
+      {
+        Type: String,
+      },
+    ],
+    price: {
+      type: Number,
+      required: true,
+    },
+    sku: {
+      type: String,
+      index: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
+    },
+    tags: [
+      {
+        type: String,
+        index: true,
+      },
+    ],
+    visible: {
+      type: Boolean,
+      default: true,
+    },
+    variants: [variantSchema],
+    seo: {
+      title: String,
+      description: String,
+    },
+  },
+  { timestamps: true }
+);
+
+productSchema.index({ title: "text", description: "text", tags: 1 });
+
+export const Product = mongoose.model("products", productSchema);
